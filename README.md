d996cf98a846bc2bed7d15cffb6e9416e5a236fd
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-07-08 20:34:58 +0800

    Add initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# qwest
+
+A web application where hosts manage challenges for participants to join and earn badges.
+
+The name is inspired from this [comic](https://swordscomic.com/comic/CDI/) :)
---
+++ src/main/java/com/jgarivera/qwest/QwestApplication.java
@@ -0,0 +1,13 @@
+package com.jgarivera.qwest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QwestApplication.class, args);
+	}
+
+}
