ec27dd851653e9f47fb4789b9883fe39e7abc110
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-07-21 18:31:27 +0800

    Add test database configuration

---
+++ src/test/java/com/jgarivera/qwest/TestDatabaseConfiguration.java
@@ -0,0 +1,18 @@
+package com.jgarivera.qwest;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration
+public class TestDatabaseConfiguration {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>(
+                "postgres:16.3-alpine3.20"
+        );
+    }
+}
