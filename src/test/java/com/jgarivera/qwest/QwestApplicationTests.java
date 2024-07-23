22186320fdb6c2836bc38305e167517a23367d98
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-07-23 19:52:35 +0800

    Import test database configuration in context loading test

--- src/test/java/com/jgarivera/qwest/QwestApplicationTests.java
+++ src/test/java/com/jgarivera/qwest/QwestApplicationTests.java
@@ -2,12 +2,14 @@ package com.jgarivera.qwest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestDatabaseConfiguration.class)
 class QwestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
