8032600ca92d109c2e9c3605e821141399258112
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-07-25 19:08:13 +0800

    Add UUIDFactory bean in TestDatabaseConfiguration

--- src/test/java/com/jgarivera/qwest/TestDatabaseConfiguration.java
+++ src/test/java/com/jgarivera/qwest/TestDatabaseConfiguration.java
@@ -1,12 +1,20 @@
 package com.jgarivera.qwest;
 
+import com.jgarivera.qwest.shared.UUIDFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
 
+import java.util.UUID;
+
 @TestConfiguration
 public class TestDatabaseConfiguration {
+
+    @Bean
+    UUIDFactory testUUIDFactory() {
+        return UUID::randomUUID;
+    }
 
     @Bean
     @ServiceConnection
