3a0e67eee2065ba6959e023ba6e0723cc4378a3c
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-07-26 18:12:29 +0800

    Add page controller

---
+++ src/main/java/com/jgarivera/qwest/challenges/presentation/PageController.java
@@ -0,0 +1,36 @@
+package com.jgarivera.qwest.challenges.presentation;
+
+import com.jgarivera.qwest.challenges.domain.Challenge;
+import com.jgarivera.qwest.challenges.domain.ChallengeRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class PageController {
+
+    private final ChallengeRepository repository;
+
+    private PageController(ChallengeRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    private String index() {
+        return "pages/index";
+    }
+
+    @GetMapping("challenges")
+    private String challenges(Model model) {
+        Iterable<Challenge> challenges = repository.findAll();
+
+        model.addAttribute("challenges", challenges);
+
+        return "pages/challenges/list";
+    }
+
+    @GetMapping("challenge-details")
+    private String challengeDetails(Model model) {
+        return "pages/challenges/details";
+    }
+}
