1ad7862f186e3fd7bd5f6a41681513dd1202681d
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-08-10 16:47:30 +0800

    Add user entity and repository

---
+++ src/main/java/com/jgarivera/qwest/auth/domain/UserId.java
@@ -0,0 +1,12 @@
+package com.jgarivera.qwest.auth.domain;
+
+import org.springframework.util.Assert;
+
+import java.util.UUID;
+
+public record UserId(UUID id) {
+
+    public UserId {
+        Assert.notNull(id, "id must not be null");
+    }
+}
