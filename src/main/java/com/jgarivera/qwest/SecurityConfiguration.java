cf693e599a7657a66937e479567d0a431e1aad8b
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-08-14 19:19:54 +0800

    Add failure url in http form login configuration

--- src/main/java/com/jgarivera/qwest/SecurityConfiguration.java
+++ src/main/java/com/jgarivera/qwest/SecurityConfiguration.java
@@ -24,6 +24,7 @@ class SecurityConfiguration {
 
         http.formLogin(
                 c -> c.loginPage("/login")
+                        .failureUrl("/login?error")
         );
 
         http.logout(c -> c.logoutSuccessUrl("/"));
