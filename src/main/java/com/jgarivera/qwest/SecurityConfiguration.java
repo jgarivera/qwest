6a248ff17856fc54800b1c06ee514befa6e27342
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-08-14 19:05:15 +0800

    Add security filter chain bean definition

--- src/main/java/com/jgarivera/qwest/SecurityConfiguration.java
+++ src/main/java/com/jgarivera/qwest/SecurityConfiguration.java
@@ -5,13 +5,31 @@ import com.jgarivera.qwest.auth.domain.UserRepository;
 import com.jgarivera.qwest.auth.domain.Username;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                c -> c.requestMatchers("/challenges").authenticated()
+                        .anyRequest().permitAll()
+        );
+
+        http.formLogin(
+                c -> c.loginPage("/login")
+        );
+
+        http.logout(c -> c.logoutSuccessUrl("/"));
+
+        return http.build();
+    }
 
     @Bean
     UserDetailsService userDetailsService(UserRepository userRepository) {
