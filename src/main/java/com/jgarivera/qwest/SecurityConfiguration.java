4b520c077313b6854e8077c5c5c355bd29c6354b
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-08-14 19:52:38 +0800

    Move user details service to separate class

--- src/main/java/com/jgarivera/qwest/SecurityConfiguration.java
+++ src/main/java/com/jgarivera/qwest/SecurityConfiguration.java
@@ -1,13 +1,8 @@
 package com.jgarivera.qwest;
 
-import com.jgarivera.qwest.auth.domain.User;
-import com.jgarivera.qwest.auth.domain.UserRepository;
-import com.jgarivera.qwest.auth.domain.Username;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,23 +25,6 @@ class SecurityConfiguration {
         http.logout(c -> c.logoutSuccessUrl("/"));
 
         return http.build();
-    }
-
-    @Bean
-    UserDetailsService userDetailsService(UserRepository userRepository) {
-        return username -> {
-            if (!Username.isValid(username)) {
-                throw new UsernameNotFoundException("User not found");
-            }
-
-            User user = userRepository.findByUsername(new Username(username));
-
-            if (user == null) {
-                throw new UsernameNotFoundException(String.format("User '%s' not found", username));
-            } else {
-                return user;
-            }
-        };
     }
 
     @Bean
