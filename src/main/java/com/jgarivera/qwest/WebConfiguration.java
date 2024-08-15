9f511500eaaf7b224d8ab6858ee9e5f20f4ff92e
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-08-15 20:09:48 +0800

    Add rest client configuration

--- src/main/java/com/jgarivera/qwest/WebConfiguration.java
+++ src/main/java/com/jgarivera/qwest/WebConfiguration.java
@@ -1,11 +1,18 @@
 package com.jgarivera.qwest;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 class WebConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    RestClient restClient() {
+        return RestClient.create();
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
