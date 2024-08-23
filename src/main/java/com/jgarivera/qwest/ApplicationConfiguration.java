9d5c56102cc3eaa3bd978f9feb10b905f5837071
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-08-23 20:17:08 +0800

    Use configuration properties scan in app config

--- src/main/java/com/jgarivera/qwest/ApplicationConfiguration.java
+++ src/main/java/com/jgarivera/qwest/ApplicationConfiguration.java
@@ -1,7 +1,9 @@
 package com.jgarivera.qwest;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationPropertiesScan
 class ApplicationConfiguration {
 }
--- src/main/java/com/jgarivera/qwest/QwestApplication.java
+++ src/main/java/com/jgarivera/qwest/QwestApplication.java
@@ -1,16 +1,12 @@
 package com.jgarivera.qwest;
 
-import com.jgarivera.qwest.identity.infrastructure.AvatarSettings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AvatarSettings.class)
 public class QwestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QwestApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(QwestApplication.class, args);
+    }
 }
