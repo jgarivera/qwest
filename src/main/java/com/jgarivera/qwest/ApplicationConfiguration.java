af8604527a8afe6f5e2fa3d310ec199a22bb5a35
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-08-27 21:15:41 +0800

    Enable async in app config to support non-blocking events

--- src/main/java/com/jgarivera/qwest/ApplicationConfiguration.java
+++ src/main/java/com/jgarivera/qwest/ApplicationConfiguration.java
@@ -2,8 +2,10 @@ package com.jgarivera.qwest;
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
 @ConfigurationPropertiesScan
+@EnableAsync
 class ApplicationConfiguration {
 }
