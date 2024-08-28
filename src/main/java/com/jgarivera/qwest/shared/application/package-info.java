dc47d10fd9ed2d3403c5d84901d89fc81c22da98
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-08-28 22:12:07 +0800

    Add package-info to shared module

---
+++ src/main/java/com/jgarivera/qwest/shared/application/package-info.java
@@ -0,0 +1,3 @@
+@org.springframework.modulith.NamedInterface
+@org.jmolecules.architecture.layered.ApplicationLayer
+package com.jgarivera.qwest.shared.application;
---
+++ src/main/java/com/jgarivera/qwest/shared/infrastructure/package-info.java
@@ -0,0 +1,2 @@
+@org.jmolecules.architecture.layered.InfrastructureLayer
+package com.jgarivera.qwest.shared.infrastructure;
