5246cd712e3d25e11c8c574fd8ee18a680cabab4
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-08-31 23:06:05 +0800

    Enforce onion architecture rules in modularity tests

--- src/test/java/com/jgarivera/qwest/ModularityTests.java
+++ src/test/java/com/jgarivera/qwest/ModularityTests.java
@@ -3,19 +3,23 @@ package com.jgarivera.qwest;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import org.jmolecules.archunit.JMoleculesArchitectureRules;
 import org.jmolecules.archunit.JMoleculesDddRules;
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
 import org.springframework.modulith.docs.Documenter;
 
-@AnalyzeClasses(packages = "com.jgarivera.qwest")
+@AnalyzeClasses(packagesOf = QwestApplication.class)
 class ModularityTests {
 
     ApplicationModules modules = ApplicationModules.of(QwestApplication.class);
 
-    @Test
-    void verifies_architecture() {
+    @ArchTest
+    void verifies_architecture(JavaClasses classes) {
         modules.verify();
+
+        JMoleculesArchitectureRules.ensureOnionSimple()
+                .check(classes);
     }
 
     @ArchTest
