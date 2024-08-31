a720cd3eaac720b028e0c56671f6ac93f32ca200
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-08-31 20:47:48 +0800

    Verify DDD rules in modularity tests

--- src/test/java/com/jgarivera/qwest/ModularityTests.java
+++ src/test/java/com/jgarivera/qwest/ModularityTests.java
@@ -1,9 +1,14 @@
 package com.jgarivera.qwest;
 
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import org.jmolecules.archunit.JMoleculesDddRules;
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
 import org.springframework.modulith.docs.Documenter;
 
+@AnalyzeClasses(packages = "com.jgarivera.qwest")
 class ModularityTests {
 
     ApplicationModules modules = ApplicationModules.of(QwestApplication.class);
@@ -17,5 +22,10 @@ class ModularityTests {
     void createDocumentation() {
         new Documenter(modules, "docs/modules")
                 .writeDocumentation();
+    }
+
+    @ArchTest
+    void verifiesDddRules(JavaClasses classes) {
+        JMoleculesDddRules.all().check(classes);
     }
 }
