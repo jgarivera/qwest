f7ef858e42d9e93cf39178ccb92fd1bc4038ecbb
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-09-18 21:14:41 +0800

    Add script for notification delete button handling

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -32,3 +32,13 @@ window.onload = function() {
         html.setAttribute("data-theme", storedTheme);
     }
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+        const $notification = $delete.parentNode;
+
+        $delete.addEventListener('click', () => {
+            $notification.parentNode.removeChild($notification);
+        });
+    });
+});
